dca16303d363a412872fd854d0a87eee6f2db083
Iván Aranda <camarillo.ivan@gmail.com>
2019-07-17 11:58:22 -0500

    agregando swagger

--- src/main/java/mx/ivanaranda/sample/mvc/Application.java
+++ src/main/java/mx/ivanaranda/sample/mvc/Application.java
@@ -1,12 +1,47 @@
 package mx.ivanaranda.sample.mvc;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@Configuration
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("mx.ivanaranda.sample.mvc.controller"))              
+          .paths(PathSelectors.any())                          
+          .build()
+          .apiInfo(apiInfo());                                           
+    }
+    
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+          "Sample MVC", 
+          "Ejemplo de MVC", 
+          "1.0.0", 
+          "", 
+          null, 
+          null, null, Collections.emptyList());
+    }
 }
+
